0e9073174d7a4052959a8d8ccc1012884252c028
owokamii <us3r424815@gmail.com>
2023-07-08 12:44:39 +0800

    3

--- Zombie Human/Assets/Scripts/Weapon/Gun.cs
+++ Zombie Human/Assets/Scripts/Weapon/Gun.cs
@@ -8,9 +8,27 @@ public class Gun : MonoBehaviour
     public Transform firePoint;
     public float fireForce = 20f;
 
+    void Start()
+    {
+        StartCoroutine(Fire());
+    }
+
+    /*
     public void Fire()
     {
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         bullet.GetComponent<Rigidbody2D>().AddForce(firePoint.up * fireForce, ForceMode2D.Impulse);
+    }
+    */
+
+    IEnumerator Fire()
+    {
+        while (true)
+        {
+            GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+            bullet.GetComponent<Rigidbody2D>().AddForce(firePoint.up * fireForce, ForceMode2D.Impulse);
+
+            yield return new WaitForSeconds(2);
+        }
     }
 }
